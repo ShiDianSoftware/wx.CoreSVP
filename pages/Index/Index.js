abf0cc890e85318cead9e575b43009554e057fc1
时点软件冯成林 <nsdictionary@icloud.com>
2017-07-16 23:53:16 +0800

    update

--- pages/Index/Index.js
+++ pages/Index/Index.js
@@ -12,10 +12,11 @@ Page({
 
   btnClick: function(){
 
-    //CoreSVP.showText("加载失败，请重新登录")
-    //CoreSVP.showLoading("加载中")
-    //CoreSVP.showSuccess("登录成功")
-    //CoreSVP.showError("登录失败")
+    CoreSVP.showText("加载失败，请重新登录")
+    CoreSVP.showLoading("加载中")
+    CoreSVP.dismiss()
+    CoreSVP.showSuccess("登录成功")
+    CoreSVP.showError("登录失败")
 
   }
 
